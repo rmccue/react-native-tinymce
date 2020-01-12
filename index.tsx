428f8c1174b84ddf95dc8a6249d491b8a66a27eb
Ryan McCue <me@ryanmccue.info>
2020-01-12 21:56:36 +0000

    Add new components to public interface

--- index.tsx
+++ index.tsx
@@ -1,2 +1,4 @@
 export { default as Editor } from './Editor';
 export { default as Toolbar } from './Toolbar';
+export { default as Provider } from './Provider';
+export { default as Tools } from './Tools';
